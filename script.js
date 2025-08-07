1ab8d1de01a2f3d253824fef783a1780ce4dabcf
zoekhall <zoe.k.hall@gmail.com>
2025-08-06 19:07:36 -0500

    add click listener

--- script.js
+++ script.js
@@ -4,6 +4,35 @@ let previousNumber = 0;
 let operator = null;
 let pendingInput = false;
 
+// variables
+const display = document.getElementById('display').value;
+const calculator = document.querySelector('.calculator');
+
+// event listener
+calculator.addEventListener('click', function (event) {
+  const button = event.target;
+  if(button.matches('button')){
+    if (button.dataset.number) {
+			handleNumbers(button.dataset.number);
+		} else if (button.dataset.action) {
+			handleActions(button.dataset.action);
+		}
+  }
+    
+});
+
+//function
+function handleNumbers(num) {
+  
+}
+
+function handleActions(action) {
+  
+}
+
+
+
+
 /* 
 ON button click - 
   if click data-number
