28f43afd6d8fd844bfdc02f7d2f8bfe0a695febb
zoekhall <zoe.k.hall@gmail.com>
2025-08-14 22:46:17 -0700

    simplify getDisplayState

--- src/calculator.js
+++ src/calculator.js
@@ -1,22 +1,18 @@
 class calculatorState {
-  constructor() {
-    this.expression = ''; // current expression/input
-    this.result = ''; // calculated result
-    this.showResult = false; //whether to show the result or the expression
-    this.error = null; // current error state
-    this.cursor = 0; // cursor position
-  }
+	constructor() {
+		this.expression = ''; // current expression/input
+		this.result = ''; // calculated result
+		this.showResult = false; //whether to show the result or the expression
+		this.error = null; // current error state
+		this.cursor = 0; // cursor position
+	}
 
-  //determine what should be displayed
-  getDisplayState() {
-    if (this.error) return { top: '', bottom: this.error };
-    if (this.showResult) return { top: this.expression, bottom: this.result };
-    return { top: this.getTopLineExp(), bottom: this.getCurrentEntry() || 0 };
-    
-  }
-
-  getTopLineExp() {
-    if (!this.result) return ''
-    if (/[+\-×÷]$/.test(this.expression)) return this.expression;
-  }
+	//determine what should be displayed
+	getDisplayState() {
+		if (this.error) {
+			return { top: '', bottom: this.error };
+		} else {
+			return { top: this.expression || '', bottom: this.result || '0' };
+		}
+	}
 }
