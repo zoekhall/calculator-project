d12fe0cad077a53956b580617efcc362af2901a6
zoekhall <zoe.k.hall@gmail.com>
2025-08-14 22:47:43 -0700

    remove showResult"

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,6 @@ class calculatorState {
 	constructor() {
 		this.expression = ''; // current expression/input
 		this.result = ''; // calculated result
-		this.showResult = false; //whether to show the result or the expression
 		this.error = null; // current error state
 		this.cursor = 0; // cursor position
 	}
