b8cf9caaad5b2c658f8a25595862e89a505b707c
zoekhall <zoe.k.hall@gmail.com>
2025-08-15 21:13:38 -0500

    add addDecimal

--- src/state.js
+++ src/state.js
@@ -4,12 +4,12 @@ class calculatorState {
 		this.result = ''; // calculated result
 		this.error = null; // current error state
     this.cursor = 0; // cursor position
+		this.historyIndex = -1; // Current position in history
+    
     this.operators = ['+', '-', 'x', '/'];
-
 		// TI-30X IIS specific features
 		this.memory = { A: 0, B: 0, C: 0, D: 0, E: 0, ANS: 0 }; // 5 memory variables + ANS
 		this.history = []; // Previous entries (up/down arrows)
-		this.historyIndex = -1; // Current position in history
 		this.angleMode = 'DEG'; // DEG, RAD, GRAD
 		this.displayMode = 'FLO'; // FLO, SCI, ENG, FIX0-FIX9
 		this.constantMode = false; // K constant operations
@@ -25,7 +25,7 @@ class calculatorState {
 		}
   }
 
-  getCurrentNumber() {
+  getCurrentNum() {
     const lastOpIndex = Math.max(...(this.operators.map(op => this.expression.lastIndexOf(op))))
     return this.expression.substring(lastOpIndex + 1)
   }
@@ -36,5 +36,24 @@ class calculatorState {
 		}
     this.expression += digit; //add digit to expression string
     this.cursor = this.expression.length; 
+  }
+
+  addDecimal() {
+    if (this.error) {
+      this.error = null; 
+    }
+
+    if (!this.getCurrentNum().includes('.')) {
+      this.expression += '.';
+      this.cursor = this.expression.length; 
+    }
+  }
+
+  clear() {
+    this.expression = '';
+    this.result = '';
+		this.error = null;
+		this.cursor = 0;
+		this.historyIndex = -1;
   }
 }
