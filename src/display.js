4cf812c2ce9c8e25b01c2ce46a88bc5c7657d2a6
zoekhall <zoe.k.hall@gmail.com>
2025-08-17 23:45:46 -0700

    add update method

--- src/display.js
+++ src/display.js
@@ -3,4 +3,10 @@ class DisplayManager {
     this.topDisplay = document.getElementById('display-operation');
     this.bottomDisplay = document.getElementById('display-result');
   }
+
+  update() {
+    const displayState = window.calculatorState.getDisplayState();
+    this.topDisplay.textContent = displayState.top;
+    this.bottomDisplay.textContent = displayState.bottom;
+  }
 }
