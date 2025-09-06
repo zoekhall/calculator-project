ff7ef6b17c562167c4699b4af75e285e29abc1d4
zoekhall <zoe.k.hall@gmail.com>
2025-09-05 23:29:42 -0500

    add min of 2 #s

--- src/state.js
+++ src/state.js
@@ -177,6 +177,7 @@ class CalculatorState {
       case 'log2': return Math.log2(value);
       case 'trunc': return Math.trunc(value);
       case 'max': return Math.max(value, this.memory.ANS);
+      case 'min': return Math.min(value, this.memory.ANS);
       case 'factorial':
         if (value < 0 || value !== Math.floor(value)) return NaN;
         let result = 1;
