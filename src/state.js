ea341075f84267b81db4f2ab97deb23fe81a19c3
zoekhall <zoe.k.hall@gmail.com>
2025-09-08 23:24:48 -0500

    add pi/2

--- src/state.js
+++ src/state.js
@@ -179,6 +179,7 @@ class CalculatorState {
       case 'max': return Math.max(value, this.memory.ANS);
       case 'min': return Math.min(value, this.memory.ANS);
       case 'sqrt2': return Math.SQRT2;
+      case 'halfpi': return Math.PI / 2;
       case 'factorial':
         if (value < 0 || value !== Math.floor(value)) return NaN;
         let result = 1;
